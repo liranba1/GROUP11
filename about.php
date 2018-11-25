21440b9f473f62f80dfa704cfebc189a194df6d0
oreldayan <45327137+oreldayan@users.noreply.github.com>
2018-11-26 00:01:36 +0200

    Add files via upload

---
+++ about.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+    font-family: "Lato", sans-serif;
+}
+
+.sidenav {
+    width: 130px;
+    position: fixed;
+    z-index: 1;
+    top: 20px;
+    left: 10px;
+    background: #eee;
+    overflow-x: hidden;
+    padding: 8px 0;
+}
+
+.sidenav a {
+    padding: 6px 8px 6px 16px;
+    text-decoration: none;
+    font-size: 25px;
+    color: #2196F3;
+    display: block;
+}
+
+.sidenav a:hover {
+    color: #064579;
+}
+
+.main {
+    margin-left: 140px; /* Same width as the sidebar + left position in px */
+    font-size: 28px; /* Increased text to enable scrolling */
+    padding: 0px 10px;
+}
+
+@media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+}
+</style>
+</head>
+<body>
+
+<div class="sidenav">
+  <a href="home.php">Home</a>
+
+</div>
+
+<div class="main">
+  <h2>About Us</h2>
+  <p>This sidebar is as tall as its content (the links), and is always shown.</p>
+  <p>Scroll down the page to see the result.</p>
+  <p>The site was established by a team of 4 partners in the fashion and engineering fields.
+The site is updated on a daily basis in new fashion items manufactured in Israel..</p>
+
+</div>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,45 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+	
+
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+
+  	  <label>Username</label>
+  	  <input type="text" name="username">
+	  <br/><br/>
+
+ 
+  	  <label>Email</label>
+  	  <input type="email" name="email">
+	  <br/><br/>
+
+
+  	  <label>Password</label>
+  	  <input type="password" name="password_1">
+	  <br/><br/>
+
+
+  	  <label>Confirm password</label>
+  	  <input type="password" name="password_2">
+	  <br/><br/>
+
+  	  <button type="submit" class="btn" name="reg_user">Register</button>
+	  <br/><br/>
+
+  	<p>
+  		Already a member? <a href="login.php">Sign in</a><br/>
+		Back to <a href="home.php">Homepage</a><br/>
+  	</p>
+  </form>
+</body>
+</html>
